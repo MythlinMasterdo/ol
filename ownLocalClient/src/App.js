591d4d98af7b106aa4d4626be6681a4f0b7c6dfe
MythlinMasterdo <miles71397@gmail.com>
2017-02-07 17:32:49 -0800

    start work on pagination

--- ownLocalClient/src/App.js
+++ ownLocalClient/src/App.js
@@ -8,7 +8,8 @@ class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      businesses: []
+      businesses: [],
+      currentPage: 1
     }
   }
 
@@ -23,6 +24,31 @@ class App extends Component {
     })
   }
 
+  changePage(newPage) {
+    var config = { headers: {'page': newPage}}
+    console.log('this1 ', this.state);
+    var self = this;
+    if(newPage > this.state.currentPage) {
+      axios.get('http://ec2-54-84-251-148.compute-1.amazonaws.com/businesses', config)
+      .then(function(response) {
+        console.log('this2 ', this.state);
+        self.setState({businesses: response.data.businesses});
+      })
+      .catch(function(err) {
+        console.log('this2 ', this.state);
+        console.log('err! ', err);
+      })
+    } else {
+      axios.get('http://ec2-54-84-251-148.compute-1.amazonaws.com/businesses', config)
+      .then(function(response) {
+        self.setState({businesses: response.data.businesses});
+      })
+      .catch(function(err) {
+        console.log('err! ', err);
+      })
+    }
+  }
+
   render() {
     return (
       <div className="App">
@@ -35,6 +61,10 @@ class App extends Component {
             return (<BusinessProfile key={business.id} business={business}/>)
           })}
           {this.props.children}
+        </div>
+        <div>
+          <button onClick={() => this.changePage(this.state.currentPage - 1)}>Previous Page</button>
+          <button onClick={() => this.changePage(this.state.currentPage + 1)}>Next Page</button>
         </div>
       </div>
     );
