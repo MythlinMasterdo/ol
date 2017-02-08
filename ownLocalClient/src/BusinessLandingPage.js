68d6f3fbfacabd9d86f2a450676b3926598a2bcd
MythlinMasterdo <miles71397@gmail.com>
2017-02-07 17:00:33 -0800

    conditionally render business landing page address2

--- ownLocalClient/src/BusinessLandingPage.js
+++ ownLocalClient/src/BusinessLandingPage.js
@@ -3,13 +3,27 @@ require('./businessProfileCss.css');
 
 class BusinessLandingPage extends Component {
   render() {
-    return(
-      <div>
+    let businessInfo = null;
+    if(window.businessInfo.address2 === null) {
+      businessInfo = <div>
         <h1>{window.businessInfo.name}</h1>
         <p><img role="presentation" className='profilePhone profileIcon'/> {window.businessInfo.phone}</p>
-        <p>Address: <img role="presentation" className='profileLocation profileIcon'/>{window.businessInfo.address} {window.businessInfo.city}, {window.businessInfo.state} {window.businessInfo.country} {window.businessInfo.zip}</p>
+        <p>Address: <img role="presentation" className='profileLocation profileIcon'/> {window.businessInfo.address} {window.businessInfo.city}, {window.businessInfo.state} {window.businessInfo.country} {window.businessInfo.zip}</p>
         <p>Website: <a href={window.businessInfo.website}>{window.businessInfo.website}</a></p>
         <p>{console.log(window.businessInfo)}</p>
+      </div>
+    } else {
+      businessInfo = <div>
+        <h1>{window.businessInfo.name}</h1>
+        <p><img role="presentation" className='profilePhone profileIcon'/> {window.businessInfo.phone}</p>
+        <p>Address: <img role="presentation" className='profileLocation profileIcon'/> {window.businessInfo.address} {window.businessInfo.address2} {window.businessInfo.city}, {window.businessInfo.state} {window.businessInfo.country} {window.businessInfo.zip}</p>
+        <p>Website: <a href={window.businessInfo.website}>{window.businessInfo.website}</a></p>
+        <p>{console.log(window.businessInfo)}</p>
+      </div>
+    }
+    return(
+      <div>
+        {businessInfo}
       </div>
     )
   }
