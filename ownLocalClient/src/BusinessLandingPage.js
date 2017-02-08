39639d492298c7db568b9c7fab4e1fc29ece2be7
MythlinMasterdo <miles71397@gmail.com>
2017-02-07 16:47:14 -0800

    render data on business landing page

--- ownLocalClient/src/BusinessLandingPage.js
+++ ownLocalClient/src/BusinessLandingPage.js
@@ -1,9 +1,16 @@
 import React, { Component } from 'react';
+require('./businessProfileCss.css');
 
 class BusinessLandingPage extends Component {
   render() {
     return(
-      <p onClick={console.log(window.businessInfo)}>Hello! World</p>
+      <div>
+        <h1>{window.businessInfo.name}</h1>
+        <p><img role="presentation" className='profilePhone profileIcon'/> {window.businessInfo.phone}</p>
+        <p>Address: <img role="presentation" className='profileLocation profileIcon'/>{window.businessInfo.address} {window.businessInfo.city}, {window.businessInfo.state} {window.businessInfo.country} {window.businessInfo.zip}</p>
+        <p>Website: <a href={window.businessInfo.website}>{window.businessInfo.website}</a></p>
+        <p>{console.log(window.businessInfo)}</p>
+      </div>
     )
   }
 }
